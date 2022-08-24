3cd3ec47eade9c247c0735c607b923bc1ca85761
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-24 12:28:56 +0200

    Cleaned folders in solution and created configuration for application layer

---
+++ ProjectManagement.Application/ApplicationServicesConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ProjectManagement.Application
+{
+    public static class ApplicationServicesConfiguration
+    {
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
